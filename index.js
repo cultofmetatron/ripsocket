9c8b40033afa0eba71f724654599e1135d47cfe5
Peter de croos <peter.decroos@smartshoot.com>
2013-07-13 13:21:35 -0700

    modded

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 
-module.exports = function() {
+var engine = require('engine.io');
+
+var Router = function(server, routes) {
   
-  
+
+};
 
 
+var Router.prototype.get = function(route) {
+
+};
+
+module.exports = app
 
 
-  return {};
-
-}
